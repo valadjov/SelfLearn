1022f02485af318416d8b21ab7eecc855a17e1d1
valadzhov <valadzhov@infragistics.com>
2016-03-19 15:41:17 +0200

    Changed the class names and added methods for the bookstore.
    Changed the name of the class BookStrock to Book and ManageStock to
    Bookstore.
    Added a property of type UUID to the Book class.
    Added methods for:
    adding a book: addBook();
    listing all the books and the number of their available copies
    listBooksAndStockCount();
    selling a book: sellBook();
    removing a book: removeBook();

---
+++ src/Pragmatic/Project/BookStore/Bookstore.java
@@ -0,0 +1,49 @@
+package Pragmatic.Project.BookStore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class Bookstore {
+
+	private String COMMA_SEPARATOR = ",";
+	private String NEW_LINE = "\n";
+
+	List<Book> books = new ArrayList<Book>();
+
+	public boolean addBook(Book b) {
+		if (!books.contains(b)) {
+			books.add(b);
+			return true;
+		}
+		return false;
+	}
+
+	public void listBooksAndStockCount() {
+		if (books.size() == 0) {
+			System.out.println("There aren't any books in the bookstore.");
+			return;
+		}
+		String[] booksAndStockCount = new String[books.size()];
+		for (int i = 0; i < books.size(); i++) {
+			System.out.println("The book " + books.get(i).getTitle() + " has "
+					+ books.get(i).getStock() + " available copies in stock.");
+		}
+	}
+
+	public boolean sellBook(Book b) {
+		if (b.getStock() > 0) {
+			b.setStock(b.getStock() - 1);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean removeBook(Book b) {
+		if (books.contains(b)) {
+			books.remove(b);
+			return true;
+		}
+		return false;
+	}
+}
