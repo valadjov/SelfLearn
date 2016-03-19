011c1c247bdcef132e48632775ba186b5a5d19ce
gohv <hristovsotirovgeorgi@gmail.com>
2016-03-19 11:39:51 +0200

    SelfLearn

---
+++ src/Pragmatic/Project/BookStore/ManageStock.java
@@ -0,0 +1,23 @@
+package Pragmatic.Project.BookStore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class ManageStock {
+
+	private String COMMA_SEPARATOR = ",";
+	private String NEW_LINE = "\n";
+	
+	List<BookStock> books = new ArrayList<BookStock>();
+		
+
+	public void addBook(books.add(new BookStock("title", "author", 8, "publisher", true, 1))){
+
+		
+		
+		
+	}
+	
+
+}
---
+++ src/Pragmatic/Project/BookStore/demo.java
@@ -0,0 +1,11 @@
+package Pragmatic.Project.BookStore;
+
+public class demo {
+
+	public static void main(String[] args) {
+		ManageStock stock = new ManageStock();
+		stock.addBook(book);
+
+	}
+
+}
